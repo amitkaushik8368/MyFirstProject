0648848e164949c42da30c735e7f695990ea0987
Amit Sharma <61858459+amitkaushik8368@users.noreply.github.com>
2025-01-04 17:46:41 +0530

    Add files via upload

--- ArrayLab012.java
+++ ArrayLab012.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
 * Program to sort an array with bubble sort method
  */
@@ -6,14 +8,20 @@ public class ArrayLab012 {
         int[] array = {54, 65, 34, 76, 12, 34, 9};
         int len=array.length;
         int temp = 0;
-        for (int i=0; i<len; i++){
-            for (int j=0; j<len-i; j++){
-                if(array[j]>array[j+1]){
+        for (int i=0; i<len; i++)
+        {
+            //i=1,j<5 array = {54, 34, 65, 12, 34, 9, 76}
+            for (int j=0; j<len-i-1; j++)
+            {
+                if(array[j]>array[j+1])
+                {
                     temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] = temp;
                 }
             }
+
         }
+        System.out.println(Arrays.toString(array));
     }
 }
